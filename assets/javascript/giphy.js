1e1b3f7145a70bc33287aa6d614345ef21fef725
W Berring <wberring@dakotacom.net>
2019-06-12 23:24:12 -0700

    Revised 2nd commit w/help

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,15 @@
+// Establishing array of names of initial buttons
+
 var shows = ["Samurai Jack", "Animaniacs", "Kim Possible", "Bugs Bunny", "Bob's Burgers",
 "Bojack Horseman", "Rick and Morty", "My Little Pony", "Steven Universe", "Regular Show",
 "Power Puff Girls", "We Bare Bears", "Gravity Fails", "The Jetsons", "The Amazing World of Gumball",
 "Garfield"];
+
+// limiting the number of GIFs to fetch/display and limiting the content rating
 var numberOfGIFs = 10;
 var cutOffRating = "R";
+
+// based on size of inital button array, FOR LOOP to render buttons
 
 function renderButtons(){
 	for(var i = 0; i < shows.length; i++) {
@@ -24,6 +30,8 @@ function renderButtons(){
 
 }
 
+// add new button function based upon user input from "on.click" function
+
 function addButton(show){
 	if(shows.indexOf(show) === -1) {
 		shows.push(show);
@@ -31,6 +39,8 @@ function addButton(show){
 		renderButtons();
 	}
 }
+
+// Sets up AJAX logic and API authentication, limits, etc. to fetch and display GIFs from giphy.com
 
 function populateGIFContainer(show){
 	$.ajax({
@@ -52,7 +62,9 @@ function populateGIFContainer(show){
 		});
 		
 		$("#gif-container").addClass("solid-border");
-		$(".gif-image").unbind("click");
+        $(".gif-image").unbind("click");
+        
+        // change GIFs from still to animated based upon click of images, changing state
 		$(".gif-image").on("click", function(){
 			if($(this).attr("state") === "still") {
 				$(this).attr("state", "animated");
